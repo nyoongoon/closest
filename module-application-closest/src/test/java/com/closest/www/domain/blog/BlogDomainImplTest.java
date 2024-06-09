10286a6b6cdf99f7131dc188ce2493da13d3b1cf
nyoongoon <nyoongoon@naver.com>
2024-06-10 06:47:16 +0900

    리드미 업데이트

---
+++ module-application-closest/src/test/java/com/closest/www/domain/blog/BlogDomainImplTest.java
@@ -0,0 +1,19 @@
+package com.closest.www.domain.blog;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class BlogDomainImplTest {
+
+    @Test
+    @DisplayName("블로그 id로 블로그 찾기")
+    void test1(){
+
+    }
+
+
+}
